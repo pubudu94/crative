f70be2d248fc63f37b4784fcd97a238a14721589
Pubudu Jayasanka <pubudu.2013101@gmail.com>
2017-08-18 09:06:19 +0530

    initial commit

---
+++ src/main/java/com/drointec/springboot/springboot_tutorial/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.drointec.springboot.springboot_tutorial;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by pubudu on 8/18/17.
+ */
+@RestController
+public class HelloWorld {
+
+    @RequestMapping(value = "/")
+    public String helloPage(){
+
+        return "Hello Pubudu" ;
+    }
+}
